3eb12e38a9dca4730c35bc53aabc15ca40b727e1
Jeremy Hurpy <jeremyhurpy@gmail.com>
2024-02-13 23:38:40 +0700

    [ADD] module cpp02 ex03 first commit

---
+++ cpp_module02/ex03/Point.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 23:37:28 by jhurpy            #+#    #+#             */
+/*   Updated: 2024/02/13 23:37:29 by jhurpy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Point.hpp"
+
+Point::Point() : _x(0) , _y(0) { }
+
+Point::Point(const Point& other)
+{
+
+}
+
+Point& Point::operator=(const Point& other)
+{
+	if (this != &other)
+	{}
+	return *this;
+}
+
+Point::Point(const float x, const float y) : _x(x), _y(y) { }
+
+Point::~Point() { }
---
+++ cpp_module02/ex03/Point.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/13 23:37:33 by jhurpy            #+#    #+#             */
+/*   Updated: 2024/02/13 23:37:34 by jhurpy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef POINT_HPP
+#define POINT_HPP
+
+#include "Fixed.hpp"
+
+class Point
+{
+public:
+	Point();
+	Point(const Point& other);
+	Point& operator=(const Point& other);
+	Point(const float x, const float y);
+	~Point();
+private:
+	const Fixed _x;
+	const Fixed _y;
+};
+
+#endif
