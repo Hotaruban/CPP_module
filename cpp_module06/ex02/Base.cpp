1dc149314db13200f0766755aec3e08a8aeee505
Hotaru <jeremyhurpy@gmail.com>
2024-07-24 11:56:37 +0700

    [FIX] implement code function identify and change test

--- cpp_module06/ex02/Base.cpp
+++ cpp_module06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 16:18:48 by jhurpy            #+#    #+#             */
-/*   Updated: 2024/07/22 23:17:15 by jhurpy           ###   ########.fr       */
+/*   Updated: 2024/07/24 11:55:44 by jhurpy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,35 +20,45 @@ Base::~Base()
 Base	*generate(void)
 {
 	// instantiate a random class A, B or C
+	srand(time(NULL));
 	int random = rand() % 3;
 
-	if (random == 0)
+	switch (random)
 	{
-		std::cout << "A random class has been generated" << std::endl;
-		return new A;
+		case 0:
+		{
+			std::cout << "A random class has been generated" << std::endl;
+			return new A;
+		}
+		case 1:
+		{
+			std::cout << "B random class has been generated" << std::endl;
+			return new B;
+		}
+		case 2:
+		{
+			std::cout << "C random class has been generated" << std::endl;
+			return new C;
+		}
 	}
-	else if (random == 1)
-	{
-		std::cout << "B random class has been generated" << std::endl;
-		return new B;
-	}
-	else
-	{
-		std::cout << "C random class has been generated" << std::endl;
-		return new C;
-	}
+	return NULL;
 }
 
 void	identify(Base *ptr)
 {
-	// identify the class pointed by p
-
-	std::cout << "Pointer is pointing to a class of type ";
+	// identify the class pointed by ptr
+	if (dynamic_cast<A*>(ptr))
+		std::cout << "It's an A" << std::endl;
+	else if (dynamic_cast<B*>(ptr))
+		std::cout << "It's a B" << std::endl;
+	else if (dynamic_cast<C*>(ptr))
+		std::cout << "It's a C" << std::endl;
+	else
+		std::cout << "Itsumi Mario!, and it's not A, B or C" << std::endl;
 }
 
 void 	identify(Base &ref)
 {
-	// identify the class pointed by p
-
-	std::cout << "Pointer is pointing to a class of type ";
+	// identify the class referenced by ref
+	identify(&ref);
 }
--- cpp_module06/ex02/main.cpp
+++ cpp_module06/ex02/main.cpp
@@ -6,29 +6,21 @@
 /*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 16:14:37 by jhurpy            #+#    #+#             */
-/*   Updated: 2024/07/22 23:08:04 by jhurpy           ###   ########.fr       */
+/*   Updated: 2024/07/24 11:54:52 by jhurpy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
 
-int	main(int argc, char **argv)
+int	main(void)
 {
-	if (argc != 2 && (argv[1][0] < '1' || argv[1][0] > '7'))
-	{
-		std::cerr << "Enter a number between 1 and 7 has argument" << std::endl;
-	}
-	int n = argv[1][0] - '0';
 	// test the Base class
-	for (int i = 0; i < n; i++)
-	{
-		Base *base = generate();
-		std::cout << "Pointer is pointing to a class of type";
-		identify(base);
-		std::cout << "Reference is pointing to a class of type";
-		identify(*base);
-		delete base;
-	}
+	std::cout << "--------------------------------------" << std::endl;
+	Base *base = generate();
+	identify(base);
+	identify(*base);
+	delete base;
+	std::cout << "--------------------------------------" << std::endl;
 	return 0;
 }
 
