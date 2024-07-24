4ae0991aeb6b4743ffae060c7629962b868392e8
Hotaru <jeremyhurpy@gmail.com>
2024-07-24 20:09:25 +0700

    [ADD] implement templace and test

---
+++ cpp_module07/ex00/whatever.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/24 17:00:30 by jhurpy            #+#    #+#             */
+/*   Updated: 2024/07/24 19:14:56 by jhurpy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	return (a > b ? a : b);
+}
+
+#endif
