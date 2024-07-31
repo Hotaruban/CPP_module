c0935621700c3bcdc82f15b5f1be04483ba07a7a
Hotaru <jeremyhurpy@gmail.com>
2024-07-31 22:58:39 +0700

    [FIX] add test

--- cpp_module07/ex00/main.cpp
+++ cpp_module07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/24 19:15:21 by jhurpy            #+#    #+#             */
-/*   Updated: 2024/07/24 20:08:22 by jhurpy           ###   ########.fr       */
+/*   Updated: 2024/07/31 22:54:17 by jhurpy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 int main(void)
 {
+	std::cout << "------------------------------------" << std::endl;
 	int a = 2;
 	int b = 3;
 
@@ -24,14 +25,16 @@ int main(void)
 	std::cout << "min(a, b) = " << ::min<int>(a, b) << std::endl;
 	std::cout << "max(a, b) = " << ::max<int>(a, b) << std::endl;
 
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+	std::cout << "------------------------------------" << std::endl;
+	std::string c = "Hello";
+	std::string d = "World";
 
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	::swap<std::string>(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min(c, d) = " << ::min<std::string>(c, d) << std::endl;
 	std::cout << "max(c, d) = " << ::max<std::string>(c, d) << std::endl;
+	std::cout << "------------------------------------" << std::endl;
 
 	return (0);
 }
