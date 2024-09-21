c59126e809afb5fa9717d21324ce0f25658e1115
Hotaru <jeremyhurpy@gmail.com>
2024-09-21 22:47:43 +0700

    [FIX] new implementation of the code

--- cpp_module09/ex02/PmergeMe.hpp
+++ cpp_module09/ex02/PmergeMe.hpp
@@ -5,58 +5,152 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/26 21:49:14 by jhurpy            #+#    #+#             */
-/*   Updated: 2024/09/16 21:48:07 by jhurpy           ###   ########.fr       */
+/*   Created: 2024/09/21 20:46:25 by jhurpy            #+#    #+#             */
+/*   Updated: 2024/09/21 22:46:05 by jhurpy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
-# include "logTab.hpp"
-# include <sstream>
+# define SIZE_PRINT 100
+
 # include <iostream>
+# include <string>
+# include <sstream>
 # include <sys/time.h>
 # include <ctime>
+# include <iomanip>
 # include <vector>
 # include <list>
 
-
+template <typename T>
 class PmergeMe
 {
 	public:
-		PmergeMe(char **av, int & ac);
+		PmergeMe(T & container);
 		~PmergeMe();
 
-		void	sortVec();
-		void	sortList();
-		void	printUnsortedNumbers(char **av, int & ac);
+		void	makeIntegers(char **av, int ac);
+		void	printContainer(std::string str, size_t size);
+		void	startClock();
+		void	endClock();
+		void	returnTime();
 
 	private:
-		PmergeMe();
-		PmergeMe(const PmergeMe &other);
-		PmergeMe &operator=(const PmergeMe &other);
+		T		& _container;
+		size_t	_size;
+		clock_t	_start, _end;
 
-		template <typename T, typename TofT>
-		void	_containerOfContainers(T & container, TofT & containerOfContainers);
-		template <typename T, typename TofT>
-		void	_sortPair(TofT & cont);
-		template <typename T>
-		void	_sortContainerInt(T & cont1, T & cont2);
-		template <typename T>
-		void	_printSortedNumbers(T & cont);
-
-		size_t							_size;
-		std::vector<int>				_vector;
-		std::vector<std::pair<std::vector<int>, std::vector<int> > >	_vectorV;
-		std::list<int>					_list;
-		std::list<std::pair<std::list<int>, std::list<int> > >		_listL;
-		//struct timeval		_startTime, _endTime;
-		clock_t				_start, _end;
-
-		void	_printTime(std::string container);
-		void	_makeIntegers(char **av, int & ac);
 		bool	_isNumber(char **av, int & ac);
+		void	_generateJacobsthal(std::vector<size_t>& jacobsthal);
+		void	_generateJacobsthal(std::list<size_t>& jacobsthal);
+
+
 };
+
+template <typename T>
+PmergeMe<T>::PmergeMe(T & container) : _container(container) {}
+
+template <typename T>
+PmergeMe<T>::~PmergeMe() {}
+
+template <typename T>
+bool	PmergeMe<T>::_isNumber(char **av, int & ac)
+{
+	for (int i = 1; i < ac; i++)
+		for (int j = 0; av[i][j]; j++)
+			if (!std::isdigit(av[i][j]) && !std::isspace(av[i][j]))
+				throw std::invalid_argument("Error: invalid argument [isNumber]: " + std::string(av[i]));
+	return (true);
+}
+
+template <typename T>
+void PmergeMe<T>::makeIntegers(char **av, int ac)
+{
+	if (ac < 1)
+		throw std::invalid_argument("Usage: ./pmergme [list of integerS]");
+	_isNumber(av, ac);
+	this->_size = ac - 1;
+
+	for (int i = 1; i < ac; i++)
+	{
+		char *endptr;
+		long number = std::strtol(av[i], &endptr, 10);
+		if (*endptr != '\0' || number < 0 || number > INT_MAX)
+			throw std::invalid_argument("Error: invalid argument: " + std::string(av[i]));
+		this->_container.push_back(static_cast<int>(number));
+	}
+}
+
+template <typename T>
+void	PmergeMe<T>::printContainer(std::string str, size_t size)
+{
+	std::cout << str;
+	for (typename T::iterator it = this->_container.begin(); it != this->_container.end(); it++)
+	{
+		std::cout << *it << " ";
+		if (std::distance(this->_container.begin(), it) == static_cast<long>(size))
+		{
+			std::cout << "[...]";
+			break;
+		}
+	}
+	std::cout << std::endl;
+}
+
+template <typename T>
+void PmergeMe<T>::startClock() { this->_start = clock(); }
+
+template <typename T>
+void PmergeMe<T>::endClock() { this->_end = clock(); }
+
+template <typename T>
+void PmergeMe<T>::returnTime()
+{
+	double time = (double)(this->_end - this->_start) / CLOCKS_PER_SEC * 1e6;
+	std::cout << std::fixed << std::setprecision(2);
+	std::cout << "Time to process a range of " << this->_size << " elements: " << time << " us" << std::endl;
+}
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                              VECTOR SPECIALIZATION                         */
+/*                                                                            */
+/* ************************************************************************** */
+
+template <typename T>
+void PmergeMe<T>::_generateJacobsthal(std::vector<size_t>& jacobsthal) {
+	jacobsthal.push_back(1);
+	jacobsthal.push_back(3);
+
+	for (size_t i = 2; ; ++i)
+	{
+		size_t next = jacobsthal[i - 1] + 2 * jacobsthal[i - 2];
+		if (next > this->_size) break;
+		jacobsthal.push_back(next);
+	}
+}
+
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                              LIST SPECIALIZATION                           */
+/*                                                                            */
+/* ************************************************************************** */
+
+//template <typename T>
+//void PmergeMe<T>::_generateJacobsthal(std::list<size_t>& jacobsthal) {
+//	jacobsthal.push_back(1);
+//	jacobsthal.push_back(3);
+
+//	for (size_t i = 2; ; ++i)
+//	{
+//		size_t next = jacobsthal.back() + 2 * jacobsthal.front();
+//		if (next > this->_size) break;
+//		jacobsthal.push_back(next);
+//	}
+//}
+
 
 #endif
--- cpp_module09/ex02/main.cpp
+++ cpp_module09/ex02/main.cpp
@@ -5,33 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jhurpy <jhurpy@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/25 13:30:00 by jhurpy            #+#    #+#             */
-/*   Updated: 2024/09/16 22:01:18 by jhurpy           ###   ########.fr       */
+/*   Created: 2024/09/21 20:46:20 by jhurpy            #+#    #+#             */
+/*   Updated: 2024/09/21 22:45:03 by jhurpy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "PmergeMe.hpp"
+#include "PmergeMe.hpp"
 
-// Use containers std::vector and std::list because they are the most suitable for the merge sort algorithm
-// - std::vector is a sequence container representing an array that can change in size
-// - std::list is a container that supports constant time insertion and removal of elements from anywhere in the container
 
-int	main(int ac, char **av)
+int main(int argc, char **argv)
 {
+	std::vector<int> vector;
+	std::list<int> list;
+
 	try {
-		if (ac < 1)
-			throw std::invalid_argument("Usage: ./pmergme [list of integerS]");
+		PmergeMe<std::vector<int> > vectorPmergeMe(vector);
+		//PmergeMe<std::list<int> > listPmergeMe(list);
 
-		PmergeMe pmergeMe(av, ac);
+		vectorPmergeMe.startClock();
+		vectorPmergeMe.makeIntegers(argv, argc);
+		vectorPmergeMe.printContainer("Before sorting:\t", SIZE_PRINT);
+		//vectorPmergeMe.mergeInsert();
+		vectorPmergeMe.endClock();
+		vectorPmergeMe.printContainer("After sorting:\t", SIZE_PRINT);
+		vectorPmergeMe.returnTime();
 
-		pmergeMe.printUnsortedNumbers(av, ac);
+		//listPmergeMe.startClock();
+		//listPmergeMe.makeIntegers(argv, argc);
+		//listPmergeMe.printContainer("Before sorting:\t", SIZE_PRINT);
+		//listPmergeMe.mergeInsert();
+		//listPmergeMe.endClock();
+		//listPmergeMe.printContainer("After sorting:\t", SIZE_PRINT);
+		//listPmergeMe.returnTime();
 
-		pmergeMe.sortVec();
-		pmergeMe.sortList();
-
-	} catch (std::invalid_argument &e) {
+	} catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
 	}
-
-	return (0);
 }
